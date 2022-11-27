f5d13473073c3c4302739e45bc9a4a3e39161af7
Ayush Shah <95910417+AyushShah2003@users.noreply.github.com>
2022-11-27 10:06:25 -0500

    commit

---
+++ css/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int radius , area;
+ cout<<" Please Enter radius of Circle";
+
+ cin>> radius;
+
+// area=PI*radius*radius
+area= 3.14*radius*radius;
+cout<<"The area of Circle is "<<area;
+return 0;
+}
